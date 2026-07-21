33fd2e4d3c08822134a1b97bbae41f590e54f8ff
Chris Bews <chris.bews@roli.com>
2026-07-21 22:54:10 +0100

    Chore: Set up Astro

---
+++ astro.config.mjs
@@ -0,0 +1,37 @@
+// @ts-check
+import { defineConfig, fontProviders } from 'astro/config';
+
+import mdx from '@astrojs/mdx';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [mdx()],
+  fonts: [{
+    provider: fontProviders.local(),
+    name: "Geist",
+    cssVariable: "--font-geist",
+    options: {
+      variants: [
+        {
+          weight: "100 900",
+          style: "normal",
+          src: ["./src/assets/fonts/Geist-VariableFont_wght.ttf"]
+        }
+      ]
+    },
+  },
+  {
+    provider: fontProviders.local(),
+    name: "Work Sans",
+    cssVariable: "--font-worksans",
+    options: {
+      variants: [
+        {
+          weight: "100 900",
+          style: "normal",
+          src: ["./src/assets/fonts/WorkSans-VariableFont_wght.ttf"]
+        }
+      ]
+    }
+  }]
+});
