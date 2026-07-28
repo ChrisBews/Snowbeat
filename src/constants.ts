4caac94973062842fdaed4f0c9a695afb3094cbf
Chris Bews <chris.bews@roli.com>
2026-07-28 18:41:23 +0100

    Feature: Add dedicated pages for projects & extras. Also add deep linking to about and timeline on the homepage

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,5 @@ export const HEADER_HEIGHT = 78;
 export const HEADER_OFFSET = 24;
 export const HEADER_HEIGHT_SMALL = 67;
 export const HEADER_OFFSET_SMALL = 10;
+export const GITHUB_URL = "https://github.com/ChrisBews";
+export const LINKEDIN_URL = "https://www.linkedin.com/in/chrisbews/";
