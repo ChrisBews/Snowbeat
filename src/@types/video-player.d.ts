199bddea6346d842791e2a18cda964575b3a7f60
Chris Bews <chris.bews@roli.com>
2026-07-31 18:05:21 +0100

    Feature: Get the rest of the video player controls working, add 2 modes ('simple' and 'full'), and make adjustments for mobile devices

---
+++ src/@types/video-player.d.ts
@@ -0,0 +1,16 @@
+interface VideoControlsElement extends HTMLElement {
+    hide: () => void;
+    show: (includeOverlay?: boolean) => void;
+    setVideoPlaying: (isPlaying: boolean) => void;
+    setMuted: (isMuted: boolean) => void;
+    setHasStarted: (hasStarted: boolean) => void;
+    updateProgress: (currentTime: number, duration: number, bufferedEnd?: number) => void;
+    hideOverlay: () => void;
+    showOverlay: () => void;
+}
+
+interface HTMLElementEventMap {
+    'progress-change': CustomEvent<{ percentage: number }>;
+}
+
+type VideoControlsType = 'none' | 'simple' | 'full';
